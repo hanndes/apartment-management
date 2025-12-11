1633982e5830e434cd54fa251688cf34e67496f7
beyzayilmaz <yilmazbeyzza@gmail.com>
2025-12-11 14:25:33 +0300

    adminDashboard guncellendi

--- src/main/java/com/group23/apartment_management/controller/AdminDashboardController.java
+++ src/main/java/com/group23/apartment_management/controller/AdminDashboardController.java
@@ -1,9 +1,13 @@
 package com.group23.apartment_management.controller;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.group23.apartment_management.entities.Announcement;
 import com.group23.apartment_management.entities.User;
 import com.group23.apartment_management.services.AnnouncementService;
 import com.group23.apartment_management.services.ComplaintService;
@@ -32,6 +36,29 @@ public class AdminDashboardController {
             return "redirect:/login";
         }
         model.addAttribute("user", admin);   // sağ üstteki profil/bildirim için
+
+        //1.aidat tahsilat oranı kartı 
+        double collectionRate = debtService.getCurrentCollectionRate();
+        model.addAttribute("collectionRate", collectionRate);
+
+        //2.ödenen daire / toplam daire kartı
+        int totalFlats = debtService.getTotalFlatCount();
+        int paidFlats = debtService.getPaidFlatCountForCurrentPeriod();
+
+        model.addAttribute("totalFlats", totalFlats);
+        model.addAttribute("paidFlats", paidFlats);
+
+        //3.bekleyen bakım kartı (şimdilik sıfır atadım complaintsService den çekeceğiz)
+        int pendingComplaints = 0;
+        model.addAttribute("pendingComplaints", pendingComplaints);
+
+        //4.kasa bakiyesi kartı (şimdilik sıfır paymentService den çekeceğiz)
+        BigDecimal currentBalance = BigDecimal.ZERO;
+        model.addAttribute("currentBalance", currentBalance);
+
+        //5.duyurular
+        List<Announcement> announcements = announcementService.getActiveAnnouncements();
+        model.addAttribute("announcements", announcements);
 
         return "admin-dashboard";
     }
