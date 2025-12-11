62008d4b8d493b85b69872d6816a2a13120d38d8
beyzayilmaz <yilmazbeyzza@gmail.com>
2025-12-11 17:32:22 +0300

    admin dashboard kartlar guncellendi

---
+++ src/main/java/com/group23/apartment_management/controller/AnnouncementController.java
@@ -0,0 +1,11 @@
+package com.group23.apartment_management.controller;
+
+import org.springframework.stereotype.Controller;
+
+import lombok.AllArgsConstructor;
+
+@Controller
+@AllArgsConstructor
+public class AnnouncementController {
+    
+}
